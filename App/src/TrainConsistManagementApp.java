ed55ac4bb0e1d5d922b77feca8fbc56694335a9c
nehh818 <nd3208@srmist.edu.in>
2026-04-13 15:57:06 +0530

    Neha added UC10-CountTotalSeatInTrain

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,24 +1,3 @@
-/*
-MAIN CLASS - TrainConsistManagementApp
-
-* Use Case 10: Count Total Seats in Train
-
-* Description:
-* This class aggregates seating capacity of all bogies
-* into a single total using Stream reduce().
-
-* At this stage, the application:
-* - Creates bogie List
-* - Maps bogies to capacity
-* - Reduces values into total
-* - Displays total seat count
-
-* This maps aggregation Logic using reduce().
-
-* @author Neha D
-* @version 10.0
-*/
-
 import java.util.*;
 import java.util.stream.Collectors;
 
